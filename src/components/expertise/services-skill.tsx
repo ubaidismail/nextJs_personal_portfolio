e2e0dc69c6dfe5d3515a2d9bea487f746e8e8faf
Ubaid Ismail <ubaidismail@Ubaids-Air.bbrouter>
2024-10-29 18:53:21 +0500

    Dark mode issue

--- src/components/expertise/services-skill.tsx
+++ src/components/expertise/services-skill.tsx
@@ -3,10 +3,10 @@ import { HoverEffect } from "../ui/card-hover-effect";
 
 export default function CardHoverEffectDemo() {
     return (
-        <div className="max-w-5xl mx-auto px-8">
+        <div className="max-w-5xl mx-auto px-8 dark:bg-dark">
 
 
-            <h2 className="mt-[50px] text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-dark dark:text-white font-sans tracking-tight">Expertise
+            <h2 className="pt-[50px] text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-dark dark:text-white font-sans tracking-tight">Expertise
             <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
           <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
             <span className="">!</span>
