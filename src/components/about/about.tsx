b4b496e83b6301f8ec5b9907eef50b9e3f18ac79
Ubaid Ismail <ubaidismail@Ubaids-Air.bbrouter>
2024-12-26 21:29:23 +0500

    About description updated

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -12,28 +12,33 @@ export default function BackgroundBeamsWithCollisionDemo() {
   return (
     <BackgroundBeamsWithCollision>
       <div>
-      <h2 className="text-2xl pt-[50px] relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
-        About{" "}
-        <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-          <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-            <span className="">Me!</span>
+        <h2 className="text-2xl pt-[50px] relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
+          About{" "}
+          <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+            <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+              <span className="">Me!</span>
+            </div>
+            <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+              <span className="">Me!</span>
+            </div>
           </div>
-          <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-            <span className="">Me!</span>
+        </h2>
+        <div className="container mx-aut">
+          <p className="dark:text-white text-black text-sm md:text-2xl text-justify mx-4">
+
+            I'm a Software Developer with over 6 years of experience in building scalable web applications, mobile apps, and SaaS solutions. Throughout my career, I have successfully completed over 300 projects, covering the full Software Development Life Cycle (SDLC). My technical expertise includes proficiency in both LAMP and MERN stacks, as well as experience with various back-end frameworks, database management, and cloud services.
+
+            Beyond my development skills, I have also gained valuable experience in sales and business development, working closely with international clients to deliver customized, scalable solutions that align with their strategic goals. I leverage my project management expertise to ensure seamless execution, guiding clients from concept to completion while enhancing customer relationships and driving business growth.
+
+            Currently, I am expanding my skill set as a student of Data Science and Python, further enhancing my ability to deliver cutting-edge solutions. I am passionate about creating innovative products that make an impact, and I thrive in collaborative environments where technology meets business strategy.
+          </p>
+
+          <div className="pb-[60px]">
+            <MovingBorderDemo HomeButtonText={ButtonText} HomeButtonLink={ButtonLink} />
           </div>
         </div>
-      </h2>
-      <div className="container mx-aut">
-      <p className="dark:text-white text-black text-sm md:text-2xl text-center mx-4">
-        I’m a software developer with over 6 years of hands-on experience building websites, web apps, mobile apps, and SaaS solutions. I’ve completed more than 250 projects, covering everything from design, coding to testing and deployment. Skilled in LAMP and MERN stacks, I also work with various back-end frameworks, database management, and cloud services. Currently, I collaborate with international clients to deliver customized, scalable solutions, offering development and project management expertise to bring their ideas to life.
-      </p>
+      </div>
 
-      <div className="pb-[60px]">
-        <MovingBorderDemo HomeButtonText={ButtonText} HomeButtonLink={ButtonLink}  />
-      </div>
-      </div>
-      </div>
-      
     </BackgroundBeamsWithCollision>
   );
 }
