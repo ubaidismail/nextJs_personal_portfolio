9dc121e83f9ec4e017f7ceccf04938cacf9a67f3
Ubaid Ismail <ubaidismail@Ubaids-Air.bbrouter>
2025-01-08 19:07:38 +0500

    mobile responsive issue fix

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -11,7 +11,7 @@ export default function BackgroundBeamsWithCollisionDemo() {
 
   return (
     <BackgroundBeamsWithCollision>
-      <div>
+      <div className="height-100">
         <h2 className="text-2xl pt-[50px] relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
           About{" "}
           <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
