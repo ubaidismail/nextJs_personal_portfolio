8a8f0bcdd6c127bbf423b21ef33653d7b1f7920f
Ubaid Ismail <ubaidismail@Ubaids-Air.bbrouter>
2025-01-17 03:07:52 +0500

    Defining env vars in next config

--- next.config.js
+++ next.config.js
@@ -25,6 +25,10 @@ module.exports = {
               ]
           }
       ]
-  }
+  },
+  env: {
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+  },
   };
   
