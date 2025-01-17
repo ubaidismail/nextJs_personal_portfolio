60a5fe00cdbcfb28090eaf9cea7a7b61849d4158
Ubaid Ismail <ubaidismail@Ubaids-Air.bbrouter>
2025-01-17 05:16:59 +0500

    Fixing transport error gmail

--- src/app/api/like-notification/route.ts
+++ src/app/api/like-notification/route.ts
@@ -23,8 +23,8 @@ export async function POST(request: Request) {
 
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
